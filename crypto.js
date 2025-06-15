b6f870170360213a8b7ddde570f49de66dc55fdb
Mstislav2016 <mym@ranger.ru>
2025-06-15 19:18:27 +0300

    Add files via upload
    Signed-off-by: Mstislav2016 <mym@ranger.ru>

---
+++ crypto.js
@@ -0,0 +1,14 @@
+﻿
+function cryptographing(frm) {
+  const inp = frm["ftext"].value;
+  let out = "";
+
+  for (let i = 0; i < inp.length; i++) {
+    let a = inp.charCodeAt(i);
+    a = 0xFFFF ^ a;
+    out += String.fromCharCode(a);
+  }
+
+  frm["fcrypto"].value = out;
+  return false;
+}
